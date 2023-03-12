c5b06849dc179413a2c2f0f1b1240f39c386d26f
zhangjing520 <zdjoey@126.com>
2023-03-12 21:51:29 +0800

    feat: 🎸 umi3+ant pro5
    项目搭建/登录页/列表页面增删改查/权限路由配置

---
+++ antdesignpro5/config/routes.ts
@@ -0,0 +1,46 @@
+﻿export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        name: 'login',
+        path: '/user/login',
+        component: './user/login',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    //access: 'canAdmin',
+    routes: [
+      {
+        path: '/admin/user',
+        name: 'user',
+        icon: 'user',
+        component: './admin/User',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './ListTableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
+];
